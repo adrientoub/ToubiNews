391bb9644505cd730286298ecb7932982fb133ff
Adrien Toubiana <toubia_a@epita.fr>
2015-06-14 01:30:47 +0200

    Change design of news detail

--- ToubiNews/NewsDetailVC.m
+++ ToubiNews/NewsDetailVC.m
@@ -16,13 +16,15 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    NSString* formatedOutput = [self createOutput: self.news.subject
-                                           author: self.news.author
-                                          content: @"Loading"];
-    [self.newsContent loadHTMLString:formatedOutput baseURL:nil];
-    [self getNewsDetails];
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+  NSString* formatedOutput = [self createOutput: self.news.subject
+                                         author: self.news.author
+                                        content: @"Loading"];
+  NSString* path = [[NSBundle mainBundle] bundlePath];
+  NSURL* baseURL = [NSURL fileURLWithPath:path];
+  [self.newsContent loadHTMLString:formatedOutput baseURL:baseURL];
+  [self getNewsDetails];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -74,19 +76,30 @@
 
 -(NSString*)nl2br:(NSString*)content
 {
-  return [content stringByReplacingOccurrencesOfString:@"\n" withString:@"<br>\n"];
+  return [[[content stringByReplacingOccurrencesOfString:@"<"
+                                              withString:@"&lt;"]
+           stringByReplacingOccurrencesOfString:@">"
+                                     withString:@"&gt;"]
+          stringByReplacingOccurrencesOfString:@"\n"
+          withString:@"<br>\n"];
+
 }
 
 -(NSString*)createOutput:(NSString*)title author:(NSString*)author content:(NSString*)content
 {
-  NSString* html = @"<h1 style=\"text-align: center;\">%@</h1><h2>%@</h2><p>%@</p>";
-  return [NSString stringWithFormat: html, title, author, content];
+  NSString* bodystyle = @"background-color: #EDEDED;";
+  NSString* h1style = @"font-size:30px;";
+  NSString* h2style = @"font-size:20px; color: #4A4A4A; text-align: center;";
+  NSString* pstyle = @"font-size:18px;text-align:left;";
+  NSString* hrstyle = @"width:50%; border-style: solid; border-width: 3px;";
+  NSString* html = @"<body style=\"%@\"><h1 style=\"%@\"><center>%@</center></h1>\n<h2 style=\"%@\">%@</h2><hr style=\"%@\"/>\n<p style=\"%@\">%@</p>";
+  return [NSString stringWithFormat: html, bodystyle, h1style, title, h2style, author, hrstyle, pstyle, content];
 }
 
 -(NSString*)parseNews: (NSDictionary*)jsonArray
 {
-  NSString* formatedOutput = [self createOutput:[jsonArray objectForKey:@"subject"]
-                                         author:[jsonArray objectForKey:@"author"]
+  NSString* formatedOutput = [self createOutput:[self nl2br:[jsonArray objectForKey:@"subject"]]
+                                         author:[self nl2br:[jsonArray objectForKey:@"author"]]
                                         content:[self nl2br:[jsonArray objectForKey:@"content"]]];
   NSArray* children = [jsonArray objectForKey:@"children"];
   for (NSDictionary* news in children)
@@ -100,7 +113,10 @@
 - (void)parseJSON:(NSDictionary*)jsonArray
 {
   NSString* formatedOutput = [self parseNews:jsonArray];
-  [self.newsContent loadHTMLString:formatedOutput baseURL:nil];
+  NSLog(@"%@", formatedOutput);
+  NSString* path = [[NSBundle mainBundle] bundlePath];
+  NSURL* baseURL = [NSURL fileURLWithPath:path];
+  [self.newsContent loadHTMLString:formatedOutput baseURL:baseURL];
 }
 
 /*
